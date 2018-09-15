51a91e1b73cf7179c473ec5b480157e53dd6e5bf
Justin <jwhittington92@yahoo.com>
2018-09-14 23:39:44 -0400

    just html/css

---
+++ logic.js
@@ -0,0 +1,10 @@
+// click button - value appears on screen
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('.buttons').addEventListener('click', function (event) {
+        //console.log('button clicked');
+        
+    })
+
+}); 
